723b3b213967da5f2f823c480619d79222b623d3
tamaszentai <zentaitamas@gmail.com>
2020-02-07 14:39:21 +0000

    transactions on the internet (controllers, erb files)

---
+++ controllers/transactions_controller.rb
@@ -0,0 +1,12 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative( '../models/transaction.rb' )
+require_relative( '../models/merchant.rb' )
+require_relative( '../models/tag.rb' )
+also_reload( '../models/*' )
+
+
+get '/transactions' do
+  @transactions = Transaction.all
+  erb ( :"transactions/index" )
+end
