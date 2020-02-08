f0f4ad6928e1bbd89b45979662c079942035f25f
tamaszentai <zentaitamas@gmail.com>
2020-02-08 12:29:05 +0000

    tag.rb basics added

---
+++ models/tag.rb
@@ -0,0 +1,10 @@
+require_relative('../db/sql_runner')
+
+class Tag
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
